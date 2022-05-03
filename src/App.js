bba7b84c6e1dae11bfee274d6697eff5959fe2c0
seananth <seansafstrom@gmail.com>
2022-05-03 21:38:31 +0200

    basic routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+//pages
+import { Search } from "./pages/Search";
+import { Watchlist } from "./pages/Watchlist";
 
 function App() {
-  return <div>
-    <p className="font-bold">hey</p>
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Search />} />
+          <Route path="/watchlist" element={<Watchlist />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
