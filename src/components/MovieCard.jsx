2b1bc1695979d2497435b40fbbe010b64c6d82bb
seananth <seansafstrom@gmail.com>
2022-05-04 11:32:30 +0200

    card height

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,21 +4,21 @@ import { MovieControls } from "./MovieControls";
 
 export const MovieCard = ({ movie }) => {
   return (
-    <div className="border rounded-lg shadow hover:shadow-md">
-      <div className="relative overflow-hidden">
-        <img
-          src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-          alt={movie.title + " " + movie.release_date + " movie poster"}
-          className="rounded-t-lg w-full"
-        />
-
-        <p className="text-white font-bold absolute top-2 left-2 bg-amber-400 p-1 rounded">
-          {movie.vote_average}/10
-        </p>
-
-        <MovieCardOverlay movie={movie} />
+    <div>
+      <div className="border rounded-lg shadow hover:shadow-md">
+        <div className="relative overflow-hidden">
+          <img
+            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+            alt={movie.title + " " + movie.release_date + " movie poster"}
+            className="rounded-t-lg w-full"
+          />
+          <p className="text-white font-bold absolute top-2 left-2 bg-amber-400 p-1 rounded">
+            {movie.vote_average}/10
+          </p>
+          <MovieCardOverlay movie={movie} />
+        </div>
+        <MovieControls movie={movie} />
       </div>
-      <MovieControls movie={movie} />
     </div>
   );
 };
