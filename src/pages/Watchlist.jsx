4e1d220f969cb4a4d4816aac971f01f396e2740d
seananth <seansafstrom@gmail.com>
2022-05-04 10:59:32 +0200

    view watchlist and favorites

--- src/pages/Watchlist.jsx
+++ src/pages/Watchlist.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 import { Container } from "../components/Container";
+//components
 import { Navbar } from "../components/Navbar";
+import { MovieCard } from "../components/MovieCard";
 
 export const Watchlist = () => {
+  const { watchlist, favorites } = useContext(GlobalContext);
+
   return (
     <div>
       <Navbar />
-      <Container>Watchlist</Container>
+      <Container>
+        <section id="Watchlist" className="py-4">
+          <p className="font-bold text-2xl">Your Watchlist</p>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {watchlist.length > 0
+              ? watchlist.map((movie) => (
+                  <MovieCard movie={movie} key={movie.id} />
+                ))
+              : null}
+          </div>
+        </section>
+        <section id="Watchlist" className="py-4">
+          <p className="font-bold text-2xl">Your Favorites</p>
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {favorites.length > 0
+              ? favorites.map((movie) => (
+                  <MovieCard movie={movie} key={movie.id} />
+                ))
+              : null}
+          </div>
+        </section>
+      </Container>
     </div>
   );
 };
